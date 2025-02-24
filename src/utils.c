72152dcbb4cb9a6f9f22bdcd94b3ec5f24163b19
Milo Niemaz <mniemaz@z4r8p5.42lyon.fr>
2025-02-24 10:45:43 +0100

    del open_map func

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: mniemaz <mniemaz@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:59:05 by mniemaz           #+#    #+#             */
-/*   Updated: 2025/02/18 14:58:52 by mniemaz          ###   ########.fr       */
+/*   Updated: 2025/02/21 17:53:27 by mniemaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,6 @@ void	print_msg(enum e_msg_ids id)
 		write(2, "Error: Please provide a map file.\n", 34);
 	if (id == ERROR_TOO_MANY_ARGS)
 		write(2, "Error: Too many arguments provided.\n", 37);
-}
-
-int	open_map_file(char *filename)
-{
-	int	fd;
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		perror("Error opening file");
-	return (fd);
 }
 
 int	get_max_row_len(t_map *map)
